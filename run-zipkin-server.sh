575a2f3118d5717d3afd99f776dc6a45a67a492c
Ron Kurr <kurr@kurron.org>
2017-04-06 10:33:23 -0400

    Use newer JVM settings

--- run-zipkin-server.sh
+++ run-zipkin-server.sh
@@ -7,14 +7,14 @@ ES_DOMAIN=${1:-phoenix}
 docker run --detach \
            --name zipkin \
            --publish 9411:9411 \
-           --env JAVA_OPTS='-Xms512m -Xmx512m -Djava.awt.headless=true' \
+           --env JAVA_OPTS='-XX:MaxRAM=512m -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -Dsun.net.inetaddr.ttl=60 -Djava.awt.headless=true' \
            --env STORAGE_TYPE=elasticsearch \
            --env ES_AWS_DOMAIN=${ES_DOMAIN} \
            --volume ${HOME}/.aws:/root/.aws:ro \
            openzipkin/zipkin
 
 # optional: tail the logs
-docker logs --follow zipkin
+#docker logs --follow zipkin
 
 # optional: open the zipkin UI in the default web browser
 xdg-open http://localhost:9411
